df0341378febfbc77aeb696690028e3b0f599ab6
Y <72870608+SunnyChing110830@users.noreply.github.com>
2021-11-20 20:58:07 +0800

    Add files via upload

--- hw1.py
+++ hw1.py
@@ -20,14 +20,14 @@ class Main(QDialog, ui.Ui_dialog):
         self.toolButton_5.clicked.connect(self.hw2_1)
         self.toolButton_6.clicked.connect(self.hw2_2)
         self.toolButton_7.clicked.connect(self.hw2_3)
-        #self.toolButton_9.clicked.connect(self.hw3_1)
-        #self.toolButton_10.clicked.connect(self.hw3_2)
-        #self.toolButton_11.clicked.connect(self.hw3_3)
-        #self.toolButton_12.clicked.connect(self.hw3_4)
-        #self.toolButton_13.clicked.connect(self.hw4_1)
-        #self.toolButton_14.clicked.connect(self.hw4_2)
-        #self.toolButton_15.clicked.connect(self.hw4_3)
-        #self.toolButton_16.clicked.connect(self.hw4_4)
+        self.toolButton_9.clicked.connect(self.hw3_1)
+        self.toolButton_10.clicked.connect(self.hw3_2)
+        self.toolButton_11.clicked.connect(self.hw3_3)
+        self.toolButton_12.clicked.connect(self.hw3_4)
+        self.toolButton_13.clicked.connect(self.hw4_1)
+        self.toolButton_14.clicked.connect(self.hw4_2)
+        self.toolButton_15.clicked.connect(self.hw4_3)
+        self.toolButton_16.clicked.connect(self.hw4_4)
     def hw1_1(self):
         img = cv2.imread('Dataset_OpenCvDl_Hw1\Q1_Image\Sun.jpg')
         cv2.imshow('LoadImage',img)
@@ -77,13 +77,96 @@ class Main(QDialog, ui.Ui_dialog):
         cv2.imshow('Median(5x5)',blur5)
     def hw3_1(self):
         img = cv2.imread('Dataset_OpenCvDl_Hw1\Q3_Image\House.jpg')
-    #def hw3_2(self):
-    #def hw3_3(self):
-    #def hw3_4(self):
-    #def hw4_1(self):
-    #def hw4_2(self):
-    #def hw4_3(self):
-    #def hw4_4(self):
+        imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+        Ginit = [(-1,1),(0,1),(1,1),
+                 (-1,0),(0,0),(1,0),
+                 (-1,-1),(0,-1),(1,-1)]
+        Gfilter = Ginit
+        SUM = 0
+        for i in range(9):
+            (x,y) = Ginit[i]
+            Gfilter[i] = np.exp(-(x**2+y**2)/(2*0.5))/(2*np.pi*0.5) #取σ^2=0.5
+            SUM += Gfilter[i]
+        Gnorm = np.array(Gfilter)/SUM
+        global Gblur
+        Gblur = np.array(imgGray).copy()
+        for i in range(1,Gblur.shape[0]-1):
+            for j in range(1,Gblur.shape[1]-1):
+                temp = 0
+                for a in range(i-1,i+2):
+                    for b in range(j-1,j+2):
+                        temp += imgGray[a][b]*Gnorm[(a-i+1)*3+(b-j+1)]
+                Gblur[i][j] = temp
+        cv2.imshow('Gaussian Blur',Gblur)
+    def hw3_2(self):
+        Xfilter = [-1,0,1,
+                   -2,0,2,
+                   -1,0,1]
+        Xnorm = np.array(Xfilter)
+        global sobX
+        sobX = np.array(Gblur).copy()/255
+        for i in range(1,Gblur.shape[0]-1):
+            for j in range(1,Gblur.shape[1]-1):
+                temp = 0
+                for a in range(i-1,i+2):
+                    for b in range(j-1,j+2):
+                        temp += Gblur[a][b]*Xnorm[(a-i+1)*3+(b-j+1)]
+                sobX[i][j] = temp/255
+        cv2.imshow('Sobel X',sobX)
+    def hw3_3(self):
+        Yfilter = [1,2,1,
+                   0,0,0,
+                   -1,-2,-1]
+        Ynorm = np.array(Yfilter)
+        global sobY
+        sobY = np.array(Gblur).copy()/255
+        for i in range(1,Gblur.shape[0]-1):
+            for j in range(1,Gblur.shape[1]-1):
+                temp = 0
+                for a in range(i-1,i+2):
+                    for b in range(j-1,j+2):
+                        temp += Gblur[a][b]*Ynorm[(a-i+1)*3+(b-j+1)]
+                sobY[i][j] = temp/255
+        cv2.imshow('Sobel Y',sobY)
+    def hw3_4(self):
+        sobXnp = np.array(sobX)
+        sobYnp = np.array(sobY)
+        Mag = ((sobXnp**2+sobYnp**2))**(0.5)
+        cv2.imshow('Magnitude',Mag)
+    def hw4_1(self):
+        img = cv2.imread('Dataset_OpenCvDl_Hw1\Q4_Image\SQUARE-01.png')
+        reHeight = 256
+        reWidth = 256
+        global imgRe
+        imgRe = cv2.resize(img,(reHeight,reWidth),interpolation=cv2.INTER_AREA)
+        cv2.imshow('img_Resized',imgRe)
+    def hw4_2(self):
+        global winX,winY
+        winX,winY=400,300
+        global Xmov,Ymov
+        Xmov,Ymov = 0,60
+        m = np.float32([[1,0,Xmov],[0,1,Ymov]])
+        global imgTr
+        imgTr = cv2.warpAffine(imgRe,m,(winX,winY))
+        cv2.imshow('img_Resized',imgRe)
+        cv2.imshow('img_Translation',imgTr)
+    def hw4_3(self):
+        angle = 10
+        scale = 0.5
+        m = cv2.getRotationMatrix2D((imgTr.shape[0]/2,imgTr.shape[1]/2),10,0.5)
+        global imgRo
+        imgRo = cv2.warpAffine(imgTr,m,(winX,winY))
+        cv2.imshow('img_Rotation,Scaling',imgRo)
+    def hw4_4(self):
+        oldL = [[50,50],
+                [200,50],
+                [50,200]]
+        newL = [[10,100],
+                [200,50],
+                [100,250]]
+        m = cv2.getAffineTransform(np.float32(oldL),np.float32(newL))
+        imgSh = cv2.warpAffine(imgRo,m,(winX,winY))
+        cv2.imshow('img_Shearing',imgSh)
 
 if __name__ == '__main__':
   import sys
