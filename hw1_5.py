ffe68e3f4b5247fb751e8937b5d1a2c8e6e8e122
Y <72870608+SunnyChing110830@users.noreply.github.com>
2021-11-24 00:15:15 +0800

    Add files via upload

--- hw1_5.py
+++ hw1_5.py
@@ -22,16 +22,16 @@ class Main(QDialog, ui.Ui_Dialog):
         #ssl._create_default_https_context = ssl._create_unverified_context
         global x_train, y_train, x_test, y_test
         (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
-        print('file loaded')
+        print('data loaded')
         
         self.b1.clicked.connect(self.no1)
-        #self.b2.clicked.connect(self.no2)
+        self.b2.clicked.connect(self.no2)
         self.b3.clicked.connect(self.no3)
         self.b4.clicked.connect(self.no4)
-        #self.b5.clicked.connect(self.no5)
+        self.b5.clicked.connect(self.no5)
 
     def no1(self):
-        #plt.gcf().set_size_inches(10,10)
+        global label
         label = {"[0]":'airplane',"[1]":'automobile',"[2]":'bird',"[3]":'cat',"[4]":'deer',
                  "[5]":'dog',"[6]":'frog',"[7]":'horse',"[8]":'ship',"[9]":'truck'}
         idx = np.random.randint(0,49991)
@@ -43,8 +43,13 @@ class Main(QDialog, ui.Ui_Dialog):
             img.set_xticks([]);img.set_yticks([])
         plt.show()
             
-    #def no2(self):
-        #print('Batch Size{}',format(Batchsize variable name))
+    def no2(self):
+        global BatchSize,LR,Opt
+        BatchSize = 32;LR = 0.001;Opt = keras.optimizers.SGD(learning_rate=LR)
+        print('hyperparameters:')
+        print('batch size: ',format(BatchSize))
+        print('learning rate: ',format(LR))
+        print('optimizer: ',format('SGD'))
     def no3(self):
         global model
         model = Sequential()
@@ -80,27 +85,44 @@ class Main(QDialog, ui.Ui_Dialog):
         model.add(Dense(10, activation='softmax'))
         
         model.summary()
-        print("")
     def no4(self):
         trainNor = x_train/255
         testNor = x_test/255
         trainOnehot = to_categorical(y_train)
         testOnehot = to_categorical(y_test)
 
-        model.compile(loss='categorical_crossentropy', optimizer='SGD', metrics=['accuracy'])
-        train_history = model.fit(trainNor,trainOnehot, validation_split=0.1, epochs = 20, batch_size = 32, verbose=1)
+        model.compile(loss='categorical_crossentropy', optimizer=Optimizer, metrics=['accuracy'])
+        train_history = model.fit(trainNor,trainOnehot, validation_split=0.1, epochs = 20, batch_size = BatchSize, verbose=1)
+        
         def show_train_history(train_history,train,validation):
             plt.plot(train_history.history[train])
             plt.plot(train_history.history[validation])
-            plt.title('Train History')
             plt.ylabel(train)
-            plt.xlabel('Epoch')
-            plt.legend(['train','validation'],loc='upper left')
-            plt.show()
+            plt.xlabel('epoch')
 
+        plt.subplot(2,1,1)
         show_train_history(train_history,'accuracy','val_accuracy')
+        plt.legend(['Training','Testing'],loc='lower right')
+        plt.subplot(2,1,2)
         show_train_history(train_history,'loss','val_loss')
-    #def no5(self):
+        plt.legend(['Training','Testing'],loc='upper right')
+        plt.show()
+        
+    def no5(self):
+        testNo = int(self.lineEdit.text())
+        count = [0 for i in range(10)]
+        for i in range(len(y_test)):
+          if y_test[i] == y_test[testNo]:
+            count[prediction[i]] += 1
+        plt.figure(0)
+        plt.imshow(x_test[testNo],cmap='binary')
+        plt.figure(1)
+        x = [i for i in range(10)]
+        xTicks = ['plane','car','bird','cat','deer','dog','frog','horse','ship','truck']
+        plt.bar(x, count, width=0.85, bottom=None, align='center', data=None, color='black')
+        plt.xticks(x,xTicks)
+        plt.xlabel('prediction')
+        plt.show()
 
 if __name__ == '__main__':
   import sys
